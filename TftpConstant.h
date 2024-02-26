025f946ef066a185988f5c9545984bca4234bc37
agrach060 <128651743+agrach060@users.noreply.github.com>
2024-02-25 16:01:15 -0800

    Create TftpConstant.h

---
+++ TftpConstant.h
@@ -0,0 +1,3 @@
+static const unsigned int MAX_PACKET_LEN = 516; // data 512 + opcode 2 + block num 2
+static const char * SERVER_FOLDER = "server-files/"; 
+static const char * CLIENT_FOLDER = "client-files/"; 
